c29c462374ff9d042ea4d8993f5dccb5b730119a
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-03-08 21:06:18 +1300

    Create PasswordLastSet.ps1

---
+++ PasswordLastSet.ps1
@@ -0,0 +1,6 @@
+#Show accounts where password was last set more than 365 days ago
+$UserSetDate = Read-Host -Prompt 'How many days back would you like to search?'
+$CurrentDate = Get-Date -Format g
+$EnteredDate = (Get-Date).AddDays(-$UserSetDate).ToString("dd/MM/yyyy")
+Write-Host "The below account/s have a password which has been set more than $UserSetDate days ago" -ForegroundColor Green
+Get-ADUser -Filter {PasswordLastSet -lt $EnteredDate} -Properties * | Select-Object Name, PasswordLastSet
